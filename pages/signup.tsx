c1548334b90f507a37391982a6c2acfc41ebab30
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2022-05-17 14:58:03 -0500

    fixed logo issue

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -58,7 +58,7 @@ const SignUp = () => {
     <div className="flex justify-center height-screen-helper">
       <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
         <div className="flex justify-center pb-12 ">
-          <Logo width="64px" height="64px" />
+          <Logo />
         </div>
         <form onSubmit={handleSignup} className="flex flex-col space-y-4">
           {message.content && (
