06179e09577e2e4cfc2ef22bc88845e375f4ffc8
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2022-05-17 07:07:19 -0500

    promode updated

--- components/ProMode.tsx
+++ components/ProMode.tsx
@@ -1,37 +1,8 @@
-import React, { useState } from 'react';
-import { createStyles, Switch, Group, useMantineColorScheme, Text, Button, Anchor } from '@mantine/core';
-import { Rocket, RocketOff } from 'tabler-icons-react';
+import React from 'react';
+import { Group, Button, Anchor } from '@mantine/core';
 import Link from 'next/link';
 
-const useStyles = createStyles((theme) => ({
-  root: {
-    position: 'relative',
-    '& *': {
-      cursor: 'pointer',
-    },
-  },
-
-  icon: {
-    pointerEvents: 'none',
-    position: 'absolute',
-    zIndex: 1,
-    top: 3,
-  },
-
-  iconLight: {
-    left: 4,
-    color: theme.white,
-  },
-
-  iconDark: {
-    right: 4,
-    color: theme.colors.gray[6],
-  },
-}));
-
 export default function ProMode() {
-  const { classes, cx } = useStyles();
-  const [checked, setChecked] = useState(false);
 
   return (
     <Group position="center">
