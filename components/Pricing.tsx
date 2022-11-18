518212cb935c4cdb5b1fda6b41c9dd55d29ee9ef
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2022-11-18 10:04:17 -0500

    ALTER TYPE pricing_plan_interval ADD VALUE 'one_time' AFTER 'year';
    ALTER TYPE pricing_plan_interval ADD VALUE 'one_time' AFTER 'year';

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -12,7 +12,7 @@ interface Props {
   products: ProductWithPrice[];
 }
 
-type BillingInterval = 'year' | 'month' | '';
+type BillingInterval = 'year' | 'month' | 'one_time';
 type SubscriptionQuantity = '1' | '2' | '3';
 
 export default function Pricing({ products }: Props) {
@@ -114,9 +114,9 @@ export default function Pricing({ products }: Props) {
             <Button
               variant="light"
               color="teal"
-              onClick={() => setBillingInterval('')}
+              onClick={() => setBillingInterval('one_time')}
               type="button"
-              className={`${billingInterval === ''
+              className={`${billingInterval === 'one_time'
                   ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
                   : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'} rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
             >
